a1162a3d2f14b2e17a35fe9224b72c047405d268
Rishabh Aggarwal <code.rishabh712@gmail.com>
2024-04-14 01:21:24 +0530

    project updated

--- app/src/main/java/com/example/cupcake/ui/viewmodels/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/ui/viewmodels/OrderViewModel.kt
@@ -10,7 +10,7 @@ import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
 
-private const val PRICE_PER_CUPCAKE = 2.00
+private const val PRICE_PER_CUPCAKE = 15.00
 private const val PRICE_FOR_SAME_DAY_PICKUP = 3.00
 
 
